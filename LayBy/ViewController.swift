b11df1aadbb7c3ecff010325130ef16ce85efba9
ceren <cerdem@geobilgi.com>
2020-03-14 18:28:42 +0300

    Initial Commit

---
+++ LayBy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LayBy
+//
+//  Created by Ceren on 14.03.2020.
+//  Copyright © 2020 Ceren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
